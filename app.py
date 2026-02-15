76a975b89825d46e059b4bb2a50471547fb92065
nishchay singh <nishchay588@gmail.com>
2026-02-16 01:59:07 +0530

    load model from HF Hub

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 import gradio as gr
 from fastai.vision.all import *
 from fastai.vision.core import PILImage as FAImage
+from huggingface_hub import hf_hub_download
 import numpy as np
-# Load the model
-try:
-    learner = load_learner("plant_diseases_model_efficientnetv2_b3_add_augmented.pkl")
-except FileNotFoundError:
-    print("Model file not found. Please ensure 'plant_diseases_model_efficientnetv2_b3_add_augmented.pkl' is in the correct directory.")
-    exit()
 
-# Define the prediction function
+MODEL_REPO = "Nishchayb9/plant-disease-efficientnetv2-b3"
+MODEL_FILENAME = "plant_diseases_model_efficientnetv2_b3_add_augmented.pkl"
+
+model_path = hf_hub_download(
+    repo_id=MODEL_REPO,
+    filename=MODEL_FILENAME
+)
+
+learner = load_learner(model_path)
 
 def classify_image(img):
     img = FAImage(img)
     preds, idx, probs = learner.predict(img)
     return {learner.dls.vocab[i]: float(probs[i]) for i in range(len(probs))}
 
-# Create the Gradio interface
 interface = gr.Interface(
     fn=classify_image,
-    inputs=gr.Image(type="pil", label="Upload Plant leaf Image"),
-    outputs=gr.Label(num_top_classes=3, label="Predicted health status"),
+    inputs=gr.Image(type="pil"),
+    outputs=gr.Label(num_top_classes=3),
     title="Plant Health Predictor",
-    description="Upload an image of a plant leaf to identify whether its healthy or not."
+    description="Upload an image of a plant leaf to identify disease."
 )
 
-# Launch the interface
 interface.launch()
