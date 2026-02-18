b29da8edcad256cb4a8d708d2094aaee1ebbfb5a
nishchay singh <nishchay588@gmail.com>
2026-02-18 19:16:38 +0530

    changed to file path instead of pil

--- app.py
+++ app.py
@@ -13,17 +13,16 @@ model_path = hf_hub_download(
 )
 
 learner = load_learner(model_path, cpu=True)
-learner.dls.after_item = Pipeline([])
-learner.dls.after_batch = Pipeline([])
 
-def classify_image(img):
+
+def classify_image(img_path):
     # FastAI's predict handles PIL images and single-item transforms correctly
-    pred, pred_idx, probs = learner.predict(img)
+    pred, pred_idx, probs = learner.predict(img_path)
     return dict(zip(learner.dls.vocab, map(float, probs)))
 
 interface = gr.Interface(
     fn=classify_image,
-    inputs=gr.Image(type="pil"),
+    inputs=gr.Image(type="filepath"),
     outputs=gr.Label(num_top_classes=3),
     title="Plant Health Predictor",
     description="Upload an image of a plant leaf to identify disease."
